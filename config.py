722d6499f2dc61a2d9d392ed2ec6e88b5dccf4d2
Joseph Damiba <joseph.damiba@plot.ly>
2020-02-13 23:31:55 -0500

    readd config to gitignore

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") 
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 16
