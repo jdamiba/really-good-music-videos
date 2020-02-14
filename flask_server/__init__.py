38dfd13059c5aecaf68416a61e5ab89ec51d16e5
Joseph Damiba <joseph.damiba@plot.ly>
2020-02-13 23:40:14 -0500

    set up configuration file

--- flask_server/__init__.py
+++ flask_server/__init__.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, login_required
-from config import Config
+from configuration import Configuration
 from flask_mail import Mail
 
 flask_server = Flask(__name__, static_folder="static")
-flask_server.config.from_object(Config)
+flask_server.config.from_object(Configuration)
 
 db = SQLAlchemy(flask_server)
 
